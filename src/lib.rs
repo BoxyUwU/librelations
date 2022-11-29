c5a6be1ea4a35c7eb618eaa36965fa3dc81a524b
Boxy <supbscripter@gmail.com>
2022-11-29 16:02:35 +0000

    assert cyclicity property of relations

--- src/lib.rs
+++ src/lib.rs
@@ -191,18 +191,58 @@ pub trait RelKind: Sized + Send + Sync + 'static {
         + AssertTreeIfAcyclic<Self, Self::SourceRestriction, Self::TargetRestriction>;
 }
 
-pub trait AssertTreeIfAcyclic<
+pub trait AssertTreeIfAcyclic<R, SourceRestriction, TargetRestriction>
+where
     R: RelKind,
     SourceRestriction: Restriction<R>,
     TargetRestriction: Restriction<R>,
->
 {
+    fn assert_cyclicity(entity: EntityMut<'_>) -> Result<(), ()>;
 }
 
-impl<R: RelKind> AssertTreeIfAcyclic<R, One, Many> for Acyclic {}
-impl<R: RelKind> AssertTreeIfAcyclic<R, Many, One> for Acyclic {}
-impl<R: RelKind> AssertTreeIfAcyclic<R, One, One> for Acyclic {}
-impl<R: RelKind, T: Restriction<R>, U: Restriction<R>> AssertTreeIfAcyclic<R, T, U> for Cyclic {}
+fn assert_cyclicity<R: RelKind>(
+    mut entity: EntityMut<'_>,
+    next_step: impl Fn(&mut EntityMut<'_>) -> Option<Entity>,
+) -> Result<(), ()> {
+    let detector_id = entity.id();
+
+    loop {
+        match next_step(&mut entity) {
+            Some(target) if target == detector_id => return Err(()),
+            Some(target) => entity = entity.into_world_mut().entity_mut(target),
+            None => return Ok(()),
+        };
+    }
+}
+
+impl<R: RelKind> AssertTreeIfAcyclic<R, One, Many> for Acyclic {
+    fn assert_cyclicity(entity: EntityMut<'_>) -> Result<(), ()> {
+        assert_cyclicity::<R>(entity, |entity| {
+            entity
+                .get_relation::<R>()
+                .map(|r| r.iter().next().unwrap().0)
+        })
+    }
+}
+impl<R: RelKind> AssertTreeIfAcyclic<R, Many, One> for Acyclic {
+    fn assert_cyclicity(entity: EntityMut<'_>) -> Result<(), ()> {
+        assert_cyclicity::<R>(entity, |entity| {
+            entity.get_noitaler::<R>().map(|r| r.iter().next().unwrap())
+        })
+    }
+}
+impl<R: RelKind> AssertTreeIfAcyclic<R, One, One> for Acyclic {
+    fn assert_cyclicity(entity: EntityMut<'_>) -> Result<(), ()> {
+        assert_cyclicity::<R>(entity, |entity| {
+            entity.get_noitaler::<R>().map(|r| r.iter().next().unwrap())
+        })
+    }
+}
+impl<R: RelKind, T: Restriction<R>, U: Restriction<R>> AssertTreeIfAcyclic<R, T, U> for Cyclic {
+    fn assert_cyclicity(_: EntityMut<'_>) -> Result<(), ()> {
+        Ok(())
+    }
+}
 
 struct Relation<T: RelKind>(<T::SourceRestriction as Restriction<T>>::RelStorage);
 
@@ -355,7 +395,14 @@ impl EntityMutExt for EntityMut<'_> {
                 }
             }
 
-            // FIXME cycle detection
+            if let Err(()) = T::Cyclicity::assert_cyclicity(world.entity_mut(source_id)) {
+                panic!(
+                    "Attempting to insert relation `{:?}` -> {} -> `{:?}` introduces a cycle.",
+                    source_id,
+                    std::any::type_name::<T>(),
+                    target_id
+                );
+            }
         });
         self
     }
